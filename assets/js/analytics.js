8a91d3ce2bd02424cca2c75dd2a7df1e9abc61d1
Jaya Vignesh J <jayavignesh324@gmail.com>
2024-10-13 13:38:52 +0530

    added analytics

---
+++ assets/js/analytics.js
@@ -0,0 +1,18 @@
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ohij3w9uok");
+
+
+    (async () => {
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = "https://www.googletagmanager.com/gtag/js?id=G-CMN6B1D2BS";
+        document.head.appendChild(script);
+      
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = function() { dataLayer.push(arguments); };
+        gtag('js', new Date());
+        gtag('config', 'G-CMN6B1D2BS');
+      })();
